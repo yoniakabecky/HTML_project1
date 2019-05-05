8c9ee48ef4db17ac9e4ac921c21bd8030b674ef7
EriMiwa <45449908+EriMiwa@users.noreply.github.com>
2019-05-05 14:11:36 -0700

    Header responsive (#27)
    * adjust common font size & header.
    
    * dalete script tag
    
    * temporary work
    
    * add js
    
    * header responsive part
    
    * fix training page's header and footer. adjust indents.
    
    * darete Unnecessary CSS and add current audition CSS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+$(function(){
+  $('#nav_toggle').click(function(){
+        $("header").toggleClass('open');
+    $("nav").fadeToggle(500);
+      });
+});
+</script>
